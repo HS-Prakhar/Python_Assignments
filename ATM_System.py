0fa5068f680519bd12c0bd1057a5b3ff2606f9cf
Prakhar Tiwari <49302988+HS-Prakhar@users.noreply.github.com>
2020-10-25 16:54:41 +0530

    Add files via upload

---
+++ ATM_System.py
@@ -0,0 +1,20 @@
+database = {
+        'eddygrant000':{
+            'name': 'Sachin',
+            'age': 22,
+            'email': 'eddygrant000@gmail.com'
+            'pin': 8888,
+            'Acc_Balance': {
+                50000
+            }
+        },
+        'sachin123':{
+            'name': 'Tanuj',
+            'age': 25,
+            'email': 'tanuj@gmail.com'
+            'pin': 1234,
+            'Acc_Balance': {
+                20000
+            }
+        }
+    }
